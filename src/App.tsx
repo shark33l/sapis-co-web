0db6c29ea41582b6572e3f6ae64b24d3ab2f4c8f
Shakeel Aman Nijabdeen <shakeelnijabdeen@gmail.com>
2024-12-15 17:35:26 +0530

    Added Test header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import { useState } from 'react'
 import './App.css'
 import Header from './components/header/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
