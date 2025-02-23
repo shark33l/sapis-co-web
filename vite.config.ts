be61533c5b4de807277474cd5f5355efa4b41fc3
Shakeel Aman Nijabdeen <shakeelnijabdeen@gmail.com>
2025-02-23 13:54:09 +0530

    Updated vite to copy 404.html when running build

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import svgr from "vite-plugin-svgr";
+import path from 'path';
+import fs from 'fs';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,5 +10,22 @@ export default defineConfig({
     react(),
     svgr(),
   ],
-  base: ""
+  base: "",
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      // Copy 404.html after build
+      plugins: [
+        {
+          name: 'copy-404',
+          writeBundle() {
+            const src = path.resolve(__dirname, '404.html');
+            const dest = path.resolve(__dirname, 'dist/404.html');
+            fs.copyFileSync(src, dest);
+            console.log('404.html copied to dist!');
+          },
+        },
+      ],
+    },
+  },
 })
