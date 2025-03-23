e829c1ea860acde4374d1e352d848af53f848b00
Shakeel Aman Nijabdeen <shakeelnijabdeen@gmail.com>
2025-03-23 21:54:40 +0530

    Removed react helmet, added static meta tags

--- src/components/Layout/LandingPage.tsx
+++ src/components/Layout/LandingPage.tsx
@@ -1,8 +1,5 @@
 import Layout from './Layout';
 
-// Components - Others
-import SEO from '../seo/SEO';
-
 // Components - Sections
 import Home from '../Sections/Home'
 import AboutUs from '../Sections/AboutUs';
@@ -10,19 +7,10 @@ import OurPurpose from '../Sections/OurPurpose';
 import Services from '../Sections/Services';
 import Contact from '../Sections/Contact';
 
-// Images
-import sapis_meta from '/images/meta/sapis_meta_image.jpg';
-
 function LandingPage() {
 
   return (
     <>
-      <SEO
-        title='Sapis | Super Alpha Petroleum Infrastructure Services'
-        description='SAPIS: KOC-approved supplier of oil & gas infrastructure services. We provide spare parts, equipment rental, transportation, and skilled manpower for drilling operations. Quality infrastructure solutions for the petroleum industry.'
-        ogImage={sapis_meta}
-        twitterImage={sapis_meta}
-      />
       <Layout>
         <Home />
         <AboutUs />
--- src/components/Layout/NotFoundPage.tsx
+++ src/components/Layout/NotFoundPage.tsx
@@ -1,22 +1,10 @@
 import Layout from './Layout';
-
-// Components - Others
-import SEO from '../seo/SEO';
-
-// Images
-import sapis_meta from '/images/meta/sapis_meta_image.jpg';
 import NotFound from '../NotFound/NotFound';
 
 function LandingPage() {
 
   return (
     <>
-      <SEO
-        title='Sapis | Super Alpha Petroleum Infrastructure Services'
-        description='SAPIS: KOC-approved supplier of oil & gas infrastructure services. We provide spare parts, equipment rental, transportation, and skilled manpower for drilling operations. Quality infrastructure solutions for the petroleum industry.'
-        ogImage={sapis_meta}
-        twitterImage={sapis_meta}
-      />
       <Layout>
         <NotFound />
       </Layout>
